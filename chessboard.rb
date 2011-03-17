ed637cf835fe69aeb91b0c6f13a34f84ec90ecdb
kevinjalbert <kevin.j.jalbert@gmail.com>
2011-03-16 22:39:07 -0400

    Restructuring of the project, first draft
    Refactor:
     * Old chessboard representation to a bitset representation
     * Valid column and row functions to the new bitset approach
    Add:
     * Function to check if the state is valid in the diagonals
     * Function to check if the board is valid among all directions
     * Function to check if a state itself is valid
     * Function to get possible states using the simple chessboard
     * Backtracking algorithm to run the whole process of exploring the
       state space
     * Ability to specify if only one solution should be found
     * Ability specify if an ordered search algorithm should be used
    Change:
     * Queen lower limit from 4 to 1

--- chessboard.rb
+++ chessboard.rb
@@ -1,48 +1,157 @@
 require 'set'
 require 'bitset'
+require 'algorithms'
 
 class Chessboard
 
   def initialize(size)
     @size = size
-    @rows = Array.new(size).map! { |column| 
-      Bitset.new(size)
-    }
+    @state = Bitset.new(size * size)
+    @backtrack_states = Containers::Stack.new
   end
 
   def print
-    puts @rows
+    puts @state
   end
 
   def valid_column(column)
     bits = 0
-    @rows.map { |row|
-      if row[column]
+
+    @size.times { |i|
+      if @state[(@size *i) + column]
         bits += 1
-      end
-      if bits == 2
-        return false
+        if bits > 1
+          return false
+        end
       end
     }
+
     return true
   end
 
   def valid_row(row)
+    row_begin = @size * (row)
     bits = 0
-    @size.times{ |bit|
-      if bit
+
+    @size.times { |i|
+      if @state[row_begin + i]
         bits += 1
-      end
-      if bits == 2
-        return false
+        if bits > 1 
+          return false
+        end
       end
     }
+
     return true
   end
 
-  # bit_string adds from the right
-  def change_row(row, bit_string)
-    @rows[row] = Bitset.from_s(bit_string)
+  # top if true means we are dealing with top row locations
+  # right if true means we are dealing with diagonal movement to the right
+  # n is the column|row position starting from the top left of the board
+  def valid_diagonal(top, right, n)
+    bits = 0
+    if top
+      if right
+        (@size - n).times { |i|
+          if @state[((@size + 1) * i) + n]
+            bits += 1
+          end
+        }
+      else
+        (n + 1).times { |i|
+          if @state[((@size - 1) * i) + n]
+            bits += 1
+          end
+        }
+      end
+    else
+      n_pos = n + ((@size - 1) * @size)
+      if right
+        (@size - n).times { |i|
+          if @state[n_pos - ((@size - 1) * i)]
+            bits += 1
+          end
+        }
+      else
+        (n + 1).times { |i|
+          if @state[n_pos - ((@size + 1) * i)]
+            bits += 1
+          end
+        }
+      end
+    end
+
+    if bits > 1
+      return false
+    else
+      return true
+    end
+  end
+
+  def all_valid
+    # Check the vertical and horizontal
+    @size.times { |i|
+      if !valid_row(i)
+        return false
+      end
+      if !valid_column(i)
+        return false
+      end
+    }
+
+    # Check the diagonal
+    @size.times { |i|
+      if !valid_diagonal(true, true, i)
+        return false
+      end
+      if !valid_diagonal(true, false, i)
+        return false
+      end
+      if !valid_diagonal(false, true, i)
+        return false
+      end
+      if !valid_diagonal(false, false, i)
+        return false
+      end
+    }
+
+    return true
+  end
+
+  def is_valid_state(state)
+    # Temporarily change state
+    temp_state = @state
+    @state = temp_state
+
+    # Check if state is valid
+    if all_valid
+      @state = temp_state
+      return true
+    else
+      @state = temp_state
+      return false
+    end
+  end
+
+  def queens_size
+    return @state.cardinality
+  end
+
+  def change_state(next_state)
+    # puts "  change state to " + next_state.to_s
+    @state = next_state
+  end
+
+  def add_backtrack_state
+    #puts "  add backtrack state " + @state.to_s
+    state = Bitset.from_s(@state.to_s)
+    @backtrack_states.push(state)
+  end
+
+  def backtrack
+    #puts "  backtrack from " + @state.to_s
+    @state = @backtrack_states.pop
+    #puts "  to " + @state.to_s
   end
 
   def get_possible_states
--- n_queens.rb
+++ n_queens.rb
@@ -9,20 +9,22 @@ require 'algorithms'
 opts = Trollop::options do
   version "n_queen_solver 0.1 (c) 2011 Kevin Jalbert"
   banner <<-EOS
-An n-queen solver that allows for some flexibility in algorithms.
+  An n-queen solver that allows for some flexibility in the approaches for solving.
 
-GitHub: <https://github.com/kevinjalbert/n_queen_solver>
+  GitHub: <https://github.com/kevinjalbert/n_queen_solver>
 
-Usage:
-        n_queens [options]
-where [options] are:
+  Usage:  n_queens [options]
+
+  where [options] are:
   EOS
 
   opt :csp, "Use constraint satisfaction problem algorithm", :default => false
+  opt :dfs, "Use a depth-first search algorithm", :default => false
   opt :ordered, "Use an ordered search algorithm", :default => false
   opt :queens, "Number of queens", :default => 4
+  opt :once, "Only find the fist goal state", :default => false
 end
-Trollop::die :queens, "must be greater then or equal to 4" if opts[:queens] < 4
+Trollop::die :queens, "must be a positive number greater then 0" if opts[:queens] < 1
 
 # Create specified board
 @board = nil
@@ -42,8 +44,75 @@ end
 
 # Create frontier (stack/queue using algorithms gem)
 @frontier = nil
+@backtrack = Containers::Stack.new
 if opts[:dfs]
   @frontier = Containers::Stack.new
 else
   @frontier = Containers::Queue.new
+end
+
+# Initially populate the frontier and set the backtrack location
+@board.get_possible_states.map { |state|
+  @frontier.push(state)
+}
+@backtrack.push(@frontier)
+
+# Loop till finished (using a backtracking algorithm)
+while true
+  # Check for goal state
+  if @board.all_valid && @board.queens_size == opts[:queens]
+    puts "==GOAL=="
+    @board.print
+
+    # Exit loop if the once flag was toggled
+    if opts[:once]
+      break
+    end
+
+    # Backtrack to last state
+    @board.backtrack
+    @frontier = @backtrack.pop
+  end
+
+  # While in an empty state, keep backtracking till a valid state
+  while true
+    # Frontier has states, or does not have anything left anymore
+    if @frontier == nil || @frontier.size != 0
+      break
+    end
+
+    # Backtrack to last state
+    @board.backtrack
+    @frontier = @backtrack.pop
+  end
+
+  # If frontier is empty then no more states to explore
+  if @frontier == nil
+    puts "==DONE=="
+    break
+  end
+
+  # Add current state to the backtrack states
+  @board.add_backtrack_state
+
+  # Change state to the next state
+  @board.change_state(@frontier.pop)
+
+  # Add the current frontier to the backtrack
+  @backtrack.push(@frontier)
+
+  # Get new frontier from current state
+  if opts[:dfs]
+    @frontier = Containers::Stack.new
+  else
+    @frontier = Containers::Queue.new
+  end
+  @board.get_possible_states.map { |state|
+    @frontier.push(state)
+  }
+
+  # Push frontier on backtrack if states are present in frontier
+  if @frontier.size != 0
+    @backtrack.push(@frontier)
+  end
 end
